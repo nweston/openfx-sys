47efa6c87aa11ae1ec6f819d469324bd428b4b86
Nathan Weston <nweston@fastmail.com>
2026-05-21 08:55:33 -0400

    Revert "Wrappers for paramGetValue"
    This reverts commit 5ff5c7136c8260bda05e0056044dc3ddbb00e0da.
    
    These don't seem to be needed. Not currently used by openfx-rs or the
    host implementation in openfx-runner.

--- build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=src/params.c");
-    cc::Build::new().file("src/params.c").compile("params");
-}
--- src/lib.rs
+++ src/lib.rs
@@ -198,37 +198,3 @@ impl std::fmt::Debug for OfxStatus {
         write!(f, "{}", self)
     }
 }
-
-type ParamGetValueFn = unsafe extern "C" fn(paramHandle: OfxParamHandle, ...) -> OfxStatus;
-
-extern "C" {
-    pub fn param_get_value_1(
-        paramGetValueFn: ParamGetValueFn,
-        param: OfxParamHandle,
-        value: *mut c_void,
-    ) -> OfxStatus;
-
-    pub fn param_get_value_2(
-        paramGetValueFn: ParamGetValueFn,
-        param: OfxParamHandle,
-        value1: *mut c_void,
-        value2: *mut c_void,
-    ) -> OfxStatus;
-
-    pub fn param_get_value_3(
-        paramGetValueFn: ParamGetValueFn,
-        param: OfxParamHandle,
-        value1: *mut c_void,
-        value2: *mut c_void,
-        value3: *mut c_void,
-    ) -> OfxStatus;
-
-    pub fn param_get_value_4(
-        paramGetValueFn: ParamGetValueFn,
-        param: OfxParamHandle,
-        value1: *mut c_void,
-        value2: *mut c_void,
-        value3: *mut c_void,
-        value4: *mut c_void,
-    ) -> OfxStatus;
-}
